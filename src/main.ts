887489c0917e2681df3b98631f29f48ac4121df0
Yonas19 <yonastsega27@gmail.com>
2026-04-02 06:50:00 +0300

    Enable local build

--- src/main.ts
+++ src/main.ts
@@ -4,10 +4,10 @@ import { AppModule } from './app.module.js';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: ['*', 'http://localhost:3001'], // Allow all origins
+    origin: ['*'], // Allow all origins
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
 
-    credentials: true, // Allow credentials
+    // credentials: true, // Allow credentials
   });
   await app.listen(process.env.PORT ?? 3000, () => {
     console.log('connected on port', process.env.PORT);
